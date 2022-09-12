d3ea5feb345c803749963c21ce782460e4d2bd55
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-09-12 23:20:54 +0530

    Create Two_elevators.cpp

---
+++ TWO_ELEVATORS/Two_elevators.cpp
@@ -0,0 +1,107 @@
+//https://codeforces.com/contest/1729/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ft first 
+#define se second 
+#define PB push_back
+#define MP make_pair
+#define nline "\n"
+#define ll long long
+/*Be patient && calm!
+Do not pray for an easy life,
+pray for the strength to endure a difficult one.
+No matter how hard it gets, NEVER GIVE UP*/
+//created by "Dhruv_Aditya"
+//大学技术学院
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS = 1e-9;
+int INF = 1000000005;
+long long INFF = 1000000000000000005LL;
+
+#ifdef TESTING
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define DEBUG
+#define VALUE(x)
+#define debug(...)
+#endif
+/*--------------------------------------------------------------------*/
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a) = (b); (a) <= (c); ++(a))
+#define FOREACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a = max(a, b)
+#define MIN(a, b) a = min(a, b)
+#define SQR(x) ((LL)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+
+void insane_fighter(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==1){
+        cout<<1<<nline;
+    }
+    else{
+    int c1=a-1;
+    if(c==1){
+    int c2=abs(b-c);
+    if(c1<c2){
+        cout<<1<<nline;
+    }
+    else if(c1==c2){
+        cout<<3<<nline;
+    }
+    else
+        cout<<2<<nline;
+    }
+    else
+       {
+        int s=abs(c-b);
+        int p=c-1;
+        int c3=p+s;
+        if(c1<c3){
+            cout<<1<<nline;
+        }
+        else if(c1==c3){
+            cout<<3<<nline;
+        }
+        else
+            cout<<2<<nline;
+
+       }
+}
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // insane_fighter();
+    int t; cin>>t;
+    TC(t)
+   {
+   		insane_fighter();
+   }
+}
